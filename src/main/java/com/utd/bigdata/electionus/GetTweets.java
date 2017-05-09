08ca1a73884cf2ff4a7d8037e055fc86e1022544
Shubham Singla <s.singla1996@gmail.com>
2017-05-09 14:53:57 +0530

    Added code files
    Added all the file with secret keys removed.

---
+++ src/main/java/com/utd/bigdata/electionus/GetTweets.java
@@ -0,0 +1,52 @@
+package com.utd.bigdata.electionus;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import twitter4j.FilterQuery;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+/*
+ * GetTweets class uses the Twitter credentials of the user to query for feeds based on the names of the politicians who are running in the race for contention of being the next US president
+ */
+
+public class GetTweets {
+	
+	/*
+	 * getTweet method usues the twitter credentials of the user to do a query on the topics - US Elections 2016 and the individual candidates
+	 * @return: void
+	 */
+	public void getTweet(){
+		ConfigurationBuilder configBuilder = new ConfigurationBuilder();
+		configBuilder.setOAuthConsumerKey("<consumerKey>");			//key removed
+		configBuilder.setOAuthConsumerSecret("<consumerSecret>");	//secret removed
+		configBuilder.setOAuthAccessToken("<accessToken>");			//accessToken removed
+		configBuilder.setOAuthAccessTokenSecret("<accessTokenSecret>");		//token secret removed
+	
+		TwitterListener twitterListener = new TwitterListener();
+		NLP.init();
+		TwitterStream streamTwitter
+		        = new TwitterStreamFactory(configBuilder.build()).getInstance();
+
+		// Registering the Listener
+		streamTwitter.addListener(twitterListener);
+
+		FilterQuery query = new FilterQuery();
+		ArrayList<String> searchFor = new ArrayList<String>();
+		String[] topics = {"#Elections2016","#DonaldTrump", "Donald Trump", "Hillary Clinton", "Bernie Sanders", "#HillaryClinton","#BernieSanders", "Ted Cruz", "John Kasich", "Gary Johnson", "Jill Stein", " Jesse Ventura"};	//Searching tweets based on the names of the politicians
+		searchFor.addAll(Arrays.asList(topics));
+		query.track(searchFor.toArray(new String[searchFor.size()]));
+
+		streamTwitter.filter(query);
+	}
+	
+	/*
+	 * main method creates an instance of class GetTweets and launches the application
+	 * @return: void
+	 */
+	public static void main(String[] args){
+		GetTweets getTweets = new GetTweets();
+		getTweets.getTweet();
+	}
+}
